963ca7cf76bdff4d294fee42db7e7848b3fd7bc5
moonsdust <emilyzqsu@gmail.com>
2020-07-13 17:09:45 -0400

    added slideshow

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+// swiper.js 
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  })
