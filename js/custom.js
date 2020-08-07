e7494d54f44d6506c19b61570c966ee768672b66
Jonathan Wu <jonathan.wu3@student.tdsb.on.ca>
2020-08-07 16:39:15 -0400

    Load nav.css before nav.html

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,14 @@
 // load in nav and footer
 $(function () {
-  $.get("nav.html", function (data) {
-    $("#nav").html(data);
-  });
-  $.get("footer.html", function (data) {
-    $("#footer").html(data);
-  });
+	$("head").append("<!--nav.css-->")
+			 .append('<link rel="stylesheet" href="/css/nav.css" />');
+	
+	$.get("nav.html", function (data) {
+		$("#nav").html(data);
+	});
+	$.get("footer.html", function (data) {
+		$("#footer").html(data);
+	});
 });
 
 // swiper.js 
